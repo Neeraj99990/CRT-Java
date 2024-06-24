72169ed259a459c4edf97d6b5f28a3299e4aa8cc
Niraj Singh <neerajsingh009424@gmail.com>
2024-06-24 19:13:22 +0530

    all done

---
+++ .vscode/Parent.java
@@ -0,0 +1,30 @@
+
+
+public class Parent {
+    void fun1(){
+        System.out.println("I am fun1 of the paraent class");
+    }
+    void fun2()
+    {
+        System.out.println("i am fun2 of the parant class");
+    }
+}
+class child extends Parent 
+{
+    void fun1(){
+        System.out.println("i am fun of child class");
+    }
+    void fun3()
+    {
+        System.out.println("i am fun3 of the child class");
+
+    }
+
+    class Test{
+        public static void main(String[] args) {
+            Parent obj = new child();
+            obj.fun1();
+        }
+    }
+   
+}
